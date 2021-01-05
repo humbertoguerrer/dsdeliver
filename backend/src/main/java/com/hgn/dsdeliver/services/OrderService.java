6bc5870313c74ea3f0353791df4e1646ab748577
humbertoguerrer <humberto.guerrer@gmail.com>
2021-01-04 23:53:35 -0300

    GET /orders

---
+++ backend/src/main/java/com/hgn/dsdeliver/services/OrderService.java
@@ -0,0 +1,25 @@
+package com.hgn.dsdeliver.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.hgn.dsdeliver.dto.OrderDTO;
+import com.hgn.dsdeliver.entities.Order;
+import com.hgn.dsdeliver.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+
+	@Transactional(readOnly = true)
+	public List<OrderDTO> findAll() {
+		List<Order> list = repository.findingOrderWithProducts();
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+}
