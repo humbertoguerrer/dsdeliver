5ffd2bd0456646426516378fcfc4a19b3ae602c3
humbertoguerrer <humberto.guerrer@gmail.com>
2021-01-05 00:24:09 -0300

    PUT /order/{id}/delivered

--- backend/src/main/java/com/hgn/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/hgn/dsdeliver/services/OrderService.java
@@ -24,23 +24,32 @@ public class OrderService {
 
 	@Autowired
 	private ProductRepository productRepository;
-	
+
 	@Transactional(readOnly = true)
 	public List<OrderDTO> findAll() {
 		List<Order> list = repository.findingOrderWithProducts();
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
 	}
-	
+
 	@Transactional
 	public OrderDTO insert(OrderDTO dto) {
-		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), Instant.now(), OrderStatus.PENDING);
-		
-	for (ProductDTO p : dto.getProducts()) {
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), Instant.now(),
+				OrderStatus.PENDING);
+
+		for (ProductDTO p : dto.getProducts()) {
 			Product product = productRepository.getOne(p.getId());
 			order.getProducts().add(product);
-	}
+		}
 		order = repository.save(order);
 		return new OrderDTO(order);
-}
+	}
 
+	@Transactional
+	public OrderDTO setDelivered(Long id) {
+		Order order = repository.getOne(id);
+		order.setStatus(OrderStatus.DELIVERED);
+		order = repository.save(order);
+		return new OrderDTO(order);
+
+	}
 }
