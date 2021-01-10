ec4392695bf90f66605d4f232fb20e80b1d08eb4
humbertoguerrer <humberto.guerrer@gmail.com>
2021-01-10 19:14:05 -0300

    Adicionada Home Page

---
+++ front-mobile/src/Header/index.tsx
@@ -0,0 +1,32 @@
+import React, { createRef } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+function Header() {
+  return (
+    <View style={styles.container}>
+      <Image source={require('../assets/logo.png')} />
+      <Text style={styles.text}>DS Deliver</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#DA5C5C',
+    height: 85,
+    paddingTop: 50,
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  text: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    lineHeight: 25,
+    letterSpacing: 0.24,
+    color: '#FFF',
+    marginLeft: 15,
+    fontFamily: 'OpenSans_700Bold',
+  }
+});
+
+export default Header;
