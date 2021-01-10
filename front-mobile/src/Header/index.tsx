36bbce6e30c184cebf20d9b7e140deef8c95429b
humbertoguerrer <humberto.guerrer@gmail.com>
2021-01-10 20:16:57 -0300

    Adicionada listagem de pedidos

--- front-mobile/src/Header/index.tsx
+++ front-mobile/src/Header/index.tsx
@@ -1,32 +1,40 @@
-import React, { createRef } from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { useNavigation } from "@react-navigation/native";
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { TouchableWithoutFeedback } from "react-native-gesture-handler";
 
 function Header() {
+  const navigation = useNavigation();
+  const handleOnPress = () => {
+    navigation.navigate('Home');
+  }
   return (
-    <View style={styles.container}>
-      <Image source={require('../assets/logo.png')} />
-      <Text style={styles.text}>DS Deliver</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={handleOnPress}>
+      <View style={styles.container}>
+        <Image source={require("../assets/logo.png")} />
+        <Text style={styles.text}>DS Deliver</Text>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#DA5C5C',
+    backgroundColor: "#DA5C5C",
     height: 85,
     paddingTop: 50,
-    flexDirection: 'row',
-    justifyContent: 'center',
+    flexDirection: "row",
+    justifyContent: "center",
   },
   text: {
-    fontWeight: 'bold',
+    fontWeight: "bold",
     fontSize: 18,
     lineHeight: 25,
     letterSpacing: 0.24,
-    color: '#FFF',
+    color: "#FFF",
     marginLeft: 15,
-    fontFamily: 'OpenSans_700Bold',
-  }
+    fontFamily: "OpenSans_700Bold",
+  },
 });
 
 export default Header;
--- front-mobile/src/Home/index.tsx
+++ front-mobile/src/Home/index.tsx
@@ -1,11 +1,17 @@
+import { useNavigation } from "@react-navigation/native";
 import React, { createRef } from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
 import { RectButton } from "react-native-gesture-handler";
+import Header from "../Header";
 
 function Home() {
-  const handleOnPress = () => {};
+  const navigation = useNavigation();
+  const handleOnPress = () => {
+    navigation.navigate('Orders');
+  };
   return (
     <>
+      <Header />
       <View style={styles.container}>
         <Image source={require("../assets/deliveryman.png")} />
         <Text style={styles.title}>
