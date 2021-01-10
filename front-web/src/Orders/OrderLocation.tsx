db7c2a76129e28659e9bfc555209441c51a0a05c
humbertoguerrer <humberto.guerrer@gmail.com>
2021-01-09 21:35:41 -0300

    Finalizado front web

--- front-web/src/Orders/OrderLocation.tsx
+++ front-web/src/Orders/OrderLocation.tsx
@@ -2,8 +2,7 @@ import { useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import AsyncSelect from "react-select/async";
 import { fetchLocalMapBox } from "../api";
-import { OrderLocationdata } from "./types";
-
+import { OrderLocationData } from "./types";
 
 const initialPosition = {
   lat: -22.3423788,
@@ -20,7 +19,7 @@ type Place = {
 };
 
 type Props = {
-  onChangeLocation: (location: OrderLocationdata) => void;
+  onChangeLocation: (location: OrderLocationData) => void;
 };
 
 function OrderLocation({ onChangeLocation }: Props) {
@@ -83,7 +82,6 @@ function OrderLocation({ onChangeLocation }: Props) {
             <Popup>{address.label}</Popup>
           </Marker>
         </MapContainer>
-        )
       </div>
     </div>
   );
