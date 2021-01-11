95a32ea1a4974f07fefa930a48af18052ab4edbf
humbertoguerrer <humberto.guerrer@gmail.com>
2021-01-10 22:36:32 -0300

    Integrado navegação por mapas para entrega de pedido

--- front-mobile/src/OrderDetails/index.tsx
+++ front-mobile/src/OrderDetails/index.tsx
@@ -1,6 +1,6 @@
 import { useNavigation } from "@react-navigation/native";
 import React from "react";
-import { Alert, StyleSheet, Text, View } from "react-native";
+import { Alert, StyleSheet, Text, View, Linking } from "react-native";
 import { RectButton } from "react-native-gesture-handler";
 import { confirmDelvery } from "../api";
 import Header from "../Header";
@@ -35,12 +35,16 @@ const handleConfirmDelivery = () => {
     })
 }
 
+const handleStartNavigation = () => {
+    Linking.openURL(`https://www.google.com/maps/dir/?api=1&travelmode=driving&dir_action=navigate&destination=${order.latitude},${order.longitude}`)
+}
+
   return (
     <>
       <Header />
       <View style={styles.container}>
         <OrderCard order={order} />
-        <RectButton style={styles.button}>
+        <RectButton style={styles.button} onPress={handleStartNavigation}>
             <Text style={styles.buttonText}>INICIAR NAVEGAÇÃO</Text>
         </RectButton>
         <RectButton style={styles.button} onPress={handleConfirmDelivery}>
